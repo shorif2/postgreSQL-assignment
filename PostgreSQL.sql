0b9005afe491987a63d58757ed37f71c2ccff1f3
shorif2 <shorifahamed571@gmail.com>
2025-05-26 01:44:34 +0600

    All the problem are solved

--- PostgreSQL.sql
+++ PostgreSQL.sql
@@ -9,6 +9,8 @@ CREATE TABLE rangers (
     region VARCHAR(100)
 )
 
+DROP TABLE rangers
+
 -- 2. species
 CREATE TABLE species (
     species_id SERIAL PRIMARY KEY,
@@ -49,20 +51,67 @@ VALUES
 (3,3,'Bamboo Grove East','2024-05-15 09:10:00','Feeding observed'),
 (1,2,'Snowfall Pass','2024-05-18 18:30:00', NULL);
 
-
---rangers
-SELECT * FROM species
-
---species
-SELECT * From rangers
-
---sightings
-SELECT * From sightings
-
 --Problem 1
 INSERT INTO rangers (name, region)
 VALUES ('Derek Fox', 'Coastal Plains')
 
 --Problem 2
-SELECT DISTINCT species_id
+SELECT COUNT(DISTINCT species_id) AS unique_species_count
 FROM sightings;
+
+--Problem 3
+SELECT * From sightings WHERE location ILIKE '%Pass%'
+
+--Problem 4
+SELECT r.name, COUNT(s.sighting_id) AS total_sightings
+FROM rangers r
+JOIN sightings s ON r.ranger_id = s.ranger_id
+GROUP BY r.name;
+
+--Problem 5
+SELECT sp.common_name
+FROM species sp
+LEFT JOIN sightings si ON sp.species_id = si.species_id
+WHERE si.species_id IS NULL;
+
+--Problem 6
+SELECT sp.common_name, s.sighting_time, r.name
+FROM sightings s
+JOIN species sp ON s.species_id = sp.species_id
+JOIN rangers r ON s.ranger_id = r.ranger_id
+ORDER BY s.sighting_time DESC
+LIMIT 2;
+
+--Problem 7
+UPDATE species
+SET conservation_status = 'Historic'
+WHERE discovery_date < '1800-01-01';
+
+--Problem 8
+SELECT 
+  sighting_id,
+  CASE
+    WHEN EXTRACT(HOUR FROM sighting_time) < 12 THEN 'Morning'
+    WHEN EXTRACT(HOUR FROM sighting_time) < 17 THEN 'Afternoon'
+    ELSE 'Evening'
+  END AS time_of_day
+FROM sightings;
+
+
+--Problem 9
+DELETE FROM rangers
+WHERE NOT EXISTS (
+  SELECT 1
+  FROM sightings
+  WHERE sightings.ranger_id = rangers.ranger_id
+);
+--rangers
+SELECT * FROM rangers
+
+--species
+SELECT * From species
+
+--sightings
+SELECT * From sightings
+
+
